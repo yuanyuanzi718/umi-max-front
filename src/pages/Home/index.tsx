6fe561c9cceda5e41dba532eaedfe35571444f82
cjh01287813 <cjh01287813@alibaba-inc.com>
2023-03-25 16:43:21 +0800

    feat(add): add

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,29 +2,30 @@ import { trim } from '@/utils/format';
 import { PageContainer } from '@ant-design/pro-components';
 import styles from './index.less';
 import { Card, Avatar, Carousel } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useModel } from '@umijs/max';
 const { Meta } = Card;
 
 const HomePage: React.FC = () => {
-  const user: any = localStorage.getItem('userinfo')
-  const [userinfo] = useState<any>(JSON.parse(user))
-  useEffect(() => {
+  const { initialState } = useModel('@@initialState');
 
-  }, []);
   return (
     <PageContainer ghost>
-      <Card
-        style={{ width: '100%', marginBottom: 100 }}
-      >
+      <Card style={{ width: '100%', marginBottom: 100 }}>
         <Meta
-          avatar={<Avatar src={userinfo?.avatar ? userinfo.avatar : "https://joesch.moe/api/v1/random"} />}
-          title={trim(userinfo?.username || 'adam')}
+          avatar={
+            <Avatar
+              src={
+                initialState?.userinfo?.avatar?.thumbUrl
+                  ? initialState?.userinfo.avatar?.thumbUrl
+                  : 'https://joesch.moe/api/v1/random'
+              }
+            />
+          }
+          title={trim(initialState?.userinfo?.username || 'adam')}
         />
       </Card>
-      <Card
-        title="公司产品"
-        style={{ width: '100%', }}
-      >
+      <Card title="公司产品" style={{ width: '100%' }}>
         <Carousel autoplay>
           <div>
             <h3 className={styles.contentStyle}>1</h3>
@@ -40,8 +41,7 @@ const HomePage: React.FC = () => {
           </div>
         </Carousel>
       </Card>
-
-    </PageContainer >
+    </PageContainer>
   );
 };
 
